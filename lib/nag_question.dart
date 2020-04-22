f5e03d61041121a5417d33bdac363df8fa359d6a
Joshua Leger <legermjoshua@gmail.com>
2020-04-21 21:09:50 -0700

    Started creating question page

---
+++ lib/nag_question.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NagQuestionPage extends StatefulWidget {
+  NagQuestionPage({Key key}) : super(key: key);
+
+  @override
+  _NagQuestionState createState() => _NagQuestionState();
+}
+
+class _NagQuestionState extends State<NagQuestionPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
